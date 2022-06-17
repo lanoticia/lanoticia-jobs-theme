172d47163d0777d1a0d5c926005e0547c7d8a165
hilbautista <96625375+hilbautista@users.noreply.github.com>
2022-06-17 15:02:43 -0500

    Agregando página quienes somos
    Antes de empezar la configuración con contenido de wordpress.

---
+++ page-quienes-somos.php
@@ -0,0 +1,79 @@
+<?php
+/* Template Name: Quiénes Somos */
+get_header();
+?>
+
+<section class="inicio-quienes-somos">
+  <div class="container text-center">
+    <div class="card-body">
+      <h1 class="card-title padding-5">¿Quiénes somos?</h1>
+      <img src="<?php echo get_stylesheet_directory_uri() ?>/img/quienessomos-graficoprincipal.jpg" class="img-fluid">
+    </div>
+  </div>
+</section>
+
+<section class="card-blue padding-5">
+  <div class="container text-center">
+    <div class="card-body">
+      <h1 class="card-title padding-5">La Noticia jobs es el nuevo portal que te ayudara a conseguir empleo rápido en Carolina del Norte, somos un portal afiliado a La Noticia, Inc.</h1>
+    </div>
+  </div>
+</section>
+
+<section class="padding-5">
+  <div class="container text-center">
+    <div class="card-body">
+      <h1 class="card-title padding-5">La Noticia jobs fue creado para brindar un mejor servicio a nuestros lectores y empleadores como una opción digital de empleos adicional a La Noticia impresa.</h1>
+    </div>
+  </div>
+</section>
+
+<section class="card-blue padding-5">
+  <div class="container text-center">
+    <div class="card-body">
+      <h1 class="card-title padding-5">La Noticia, Inc., es una organización de noticias e información en digital e impresos.</h1>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="padding-5">
+  <div class="container text-center">
+    <div class="card-body">
+      <img src="<?php echo get_stylesheet_directory_uri() ?>/img/quienessomos-enlodigital.jpg" class="img-fluid">
+      <h1 class="card-title padding-5">En digital</h1>
+      <p>
+      LaNoticia.com es un medio de comunicación local e independiente. Nuestra misión es ser la más valiosa fuente de información para la comunidad latina.
+      Nuestra audiencia prefiere LaNoticia.com porque somos locales, somos comunidad, los proveemos con noticias e información sobre temas que afectan a la comunidad latina.
+      </p>
+      <img src="<?php echo get_stylesheet_directory_uri() ?>/img/quienessomos-enimpreso.jpg" class="img-fluid">
+      <h1 class="card-text padding-5">En impreso</h1>
+      <p>
+      La Noticia, The Spanish-Language Newspaper sirve a la comunidad latina en Carolina del Norte con cuatro ediciones diferentes:
+   
+      <strong>
+      La Noticia Charlotte<br>
+      La Noticia Raleigh, Durham, Chapel Hill<br>
+      La Noticia Greensboro, Winston-Salem, High Point<br>
+      La Noticia Asheville, Hendersonville<br>
+      </strong>
+      
+      Es el periódico en español más grande entre Washington, DC y Atlanta, GA. Es el medio de comunicación en español más leído, más respetado y el más profesionalmente hecho.
+
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class="card-blue padding-5">
+  <div class="container text-center">
+    <div class="card-body">
+      <h1 class="card-title padding-5">Hemos estado sirviendo a más de 300,000 miembros de la comunidad latina en Carolina del Norte con La Noticia, The Spanish-Language Newspaper por más de 24 años y con LaNoticia.com desde el 2015.
+</h1>
+    </div>
+  </div>
+</section>
+
+<?php
+get_footer();
